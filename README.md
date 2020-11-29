a36eee9bdfa7b0d8f01ff12d0c293bd0f72b9b01
Jhony Vidal <jhony.oliveira@mulesoft.com>
2020-11-28 23:48:19 -0300

    Parallelize and show spark DataFrame example on playground

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 ### SparkLab
 
+What Spark is?
+
+Unified computing engine & libraries for distributed data processing which supports:
+- data loading
+- SQL queries
+- machine learning
+- streaming
+
+#### Misconceptions 
+Spark is not concerned with data sources:
+- files
+- Azure
+- S3
+- Hadoop/HDFS
+- Cassandra
+- Postgres
+- Kafka
+
+Spark is not part of Hadoop.
+
+### Running
 Build and start PostgreSQL container that will be used to interact with from Spark:
 ```
 docker-compose up
@@ -17,3 +38,9 @@ Go to spark-cluster in another terminal and run docker-compose up --scale spark-
 chmod +x build-images.sh
 ./build-images.sh
 ```
+
+#### Architecture
+
+[Applications] Streaming | ML | GraphX | Other libraries
+[High-level (structured) APIS] Data Frames | Datasets | Spark SQL
+[Low-Level] RDDs | Distributed variables
--- src/main/scala/Playground.scala
+++ src/main/scala/Playground.scala
@@ -1,4 +1,6 @@
-import org.apache.spark.sql.SparkSession
+
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types.{DoubleType, LongType, StringType, StructField, StructType}
 
 object Playground extends App {
 
@@ -10,5 +12,49 @@ object Playground extends App {
     .config("spark.master", "local")
     .getOrCreate()
 
+  /**
+   * SParkContext (also can be denoted sc in code) is the entry low-level point
+   * for Spark APIs, including access to Resilient Distributed Datasets(RDDs)
+   */
   val sparkContext = spark.sparkContext
+
+  /**
+   * Spark schema structure that describes a small cars DataFrame.
+   */
+  val carsSchema = StructType(Array(
+    StructField("Name", StringType),
+    StructField("Miles_per_Gallon", DoubleType),
+    StructField("Cylinders", LongType),
+    StructField("Displacement", DoubleType),
+    StructField("Horsepower", LongType),
+    StructField("Weight_in_lbs", LongType),
+    StructField("Acceleration", DoubleType),
+    StructField("Year", StringType),
+    StructField("Origin", StringType)
+  ))
+
+  /**
+   * A hard-code dummy sequence of rows describing cards for test
+   */
+  val cars = Seq(
+    Row("chevrolet chevelle malibu",18.0,8L,307.0,130L,3504L,12.0,"1970-01-01","USA"),
+    Row("buick skylark 320",15.0,8L,350.0,165L,3693L,11.5,"1970-01-01","USA"),
+    Row("plymouth satellite",18.0,8L,318.0,150L,3436L,11.0,"1970-01-01","USA"),
+    Row("amc rebel sst",16.0,8L,304.0,150L,3433L,12.0,"1970-01-01","USA"),
+    Row("ford torino",17.0,8L,302.0,140L,3449L,10.5,"1970-01-01","USA"),
+    Row("ford galaxie 500",15.0,8L,429.0,198L,4341L,10.0,"1970-01-01","USA"),
+    Row("chevrolet impala",14.0,8L,454.0,220L,4354L,9.0,"1970-01-01","USA"),
+    Row("plymouth fury iii",14.0,8L,440.0,215L,4312L,8.5,"1970-01-01","USA"),
+    Row("pontiac catalina",14.0,8L,455.0,225L,4425L,10.0,"1970-01-01","USA"),
+    Row("amc ambassador dpl",15.0,8L,390.0,190L,3850L,8.5,"1970-01-01","USA")
+  )
+
+  /**
+   * Create an RDD(think like a parallel collection) of rows
+   * From the RDD we create a DataFrame which has a number of useful querying methods
+   */
+  val carsRows = sparkContext.parallelize(cars)
+  val carsDF = spark.createDataFrame(carsRows, carsSchema)
+  carsDF.printSchema()
+  carsDF.show()
 }
